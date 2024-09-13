da9bbe29e14307c13b82d2f2d476ae27915a269a
Ivan Carlos <ivan@ivancarlos.com.br>
2024-09-13 11:33:34 -0300

    Update rclone-mariadb_web.sh

--- rclone-mariadb_web.sh
+++ rclone-mariadb_web.sh
@@ -5,13 +5,15 @@ DATABASE=db-name
 USER=db-username-or-db-root
 RCLONEREMOTE=rclone-remote-name
 BUCKET=bucket-name
-WEBDIR=/path/of-web-dir-before-webfolder
+WEBDIR=/path/of/webdir/before/webfolder
 WEBFOLDER=htdocs-or-equivalent
 RETENTION=7d
 
 mkdir -p ./backup
 
+# /opt/bitnami/mariadb/bin/mariadb-dump --defaults-extra-file=./.dbpassword.cnf -u $USER --single-transaction --skip-lock-tables --quick $DATABASE > ./backup/$FILENAMEDATE.sql
 mariadb-dump --defaults-extra-file=./.dbpassword.cnf -u $USER --single-transaction --skip-lock-tables --quick $DATABASE > ./backup/$FILENAMEDATE.sql
+
 
 tar -czf ./backup/$FILENAMEDATE-DB.tar.gz --remove-files ./backup/$FILENAMEDATE.sql
 tar -czf ./backup/$FILENAMEDATE-WEB.tar.gz -C $WEBDIR $WEBFOLDER
@@ -19,5 +21,3 @@ tar -czf ./backup/$FILENAMEDATE-WEB.tar.gz -C $WEBDIR $WEBFOLDER
 rclone move ./backup/$FILENAMEDATE-DB.tar.gz $RCLONEREMOTE:$BUCKET/
 rclone move ./backup/$FILENAMEDATE-WEB.tar.gz $RCLONEREMOTE:$BUCKET/
 rclone delete --min-age $RETENTION $RCLONEREMOTE:$BUCKET/
-
-
